557359dc370333f07fdd21c232938d80aa902ad0
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-02-07 12:32:14 -0800

    Initialize App component and render it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id='App'></div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
