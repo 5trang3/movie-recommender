cb55de04c009a8ea74d26e928df485491b1e427e
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-02-07 15:11:49 -0800

    Implement dropdown for genre selection

--- src/index.js
+++ src/index.js
@@ -2,16 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Array of IMDB genres:
+const imdbGenres = [
+  'Action',
+  'Adventure',
+  'Animation',
+  'Biography',
+  'Comedy',
+  'Crime',
+  'Documentary',
+  'Drama',
+  'Family',
+  'Fantasy',
+  'Film Noir',
+  'History',
+  'Horror',
+  'Music',
+  'Musical',
+  'Mystery',
+  'Romance',
+  'Sci-Fi',
+  'Short',
+  'Sport',
+  'Superhero',
+  'Thriller',
+  'War',
+  'Western'
+]
+
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+
+    // Get array of option elements for each genre
+    const genreOptions = this.props.imdbGenres.map(function(genre) {
+      return <option value={genre}>{genre}</option>
+    })
+
     return (
       <div id='app'>
+        <div id='genreDropdownContainer'>
+          <label id='genreDropdownLabel'>Choose a Genre:</label>
+          <select id='genreDropdown'>
+            {genreOptions}
+          </select>
+        </div>
       </div>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App imdbGenres={imdbGenres}/>, document.getElementById('root'))
