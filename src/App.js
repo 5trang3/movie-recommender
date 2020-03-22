2c96280e751a5e385456e7743226101c0025bf96
Prasant <33533156+5trang3@users.noreply.github.com>
2020-03-22 12:39:48 -0700

    Remove imdb ratings

--- src/App.js
+++ src/App.js
@@ -129,7 +129,6 @@ class Movie extends React.Component {
       <div class='movie'>
         <a href={ 'https://imdb.com/title/' + this.props.movie.id}><img class='image' src={this.props.movie.posterPath}/></a>
         <p>{this.props.movie.title}</p>
-        <div class='imdbRating score'>{this.props.movie.rating}</div>
         <div class='genreScore score'>{genreAdjustedScore}</div>
       </div>
     )
