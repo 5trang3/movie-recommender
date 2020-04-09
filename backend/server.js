2c928782c4797c425c1741862b88b707ab46f66c
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-04-09 15:55:23 -0700

    Resolve CORS errors

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.options('*', cors());
+
 // Adds the react production build to serve react requests
 
 app.use(express.static(path.join(__dirname, '../build')));
