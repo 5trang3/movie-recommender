6582d85d70e984ce6fbb97a2b321b0b3f6010c1e
Prasant <33533156+5trang3@users.noreply.github.com>
2020-04-16 22:36:06 -0700

    Remove old environment configuration variable

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
-const PORT = process.env.REACT_APP_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
