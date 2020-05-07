f9efd79c97d7747bcae47e08f5ba39b528f33d1b
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-05-06 20:06:15 -0700

    Move link to wrap around entire card element

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Badge from './Badge.js';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    '& a': {
+      textDecoration: 'none'
+    }
+  }
+});
 
 const Movie = (props) => {
     let genreAdjustedScore;
@@ -14,20 +23,21 @@ const Movie = (props) => {
       }
     }
     const [isRaised, toggleRaised] = useState(false);
+    const classes = useStyles();
     return(
-        <Box class='grid-item' style={{ height: '450px', width: '185px' }}>
+        <Box style={{ height: '450px', width: '185px' }} className={ classes.root }>
           <Badge rating={ genreAdjustedScore }/>
-          <Card onMouseEnter={ () =>  toggleRaised (true) } onMouseLeave={ () =>  toggleRaised (false) } raised={ isRaised }>
-            <a href={ 'https://imdb.com/title/' + props.movie.id}>
+          <a href={ 'https://imdb.com/title/' + props.movie.id}>
+            <Card onMouseEnter={ () =>  toggleRaised (true) } onMouseLeave={ () =>  toggleRaised (false) } raised={ isRaised }>
               <CardMedia image={ props.movie.posterPath } style={{ height: '278px', backgroundSize: 'contain'}}>
               </CardMedia>
-            </a>
-            <CardContent style={{ height: '70px'}}>
-              <Typography variant='subtitle1' style={{ textAlign: 'center' }}>
-                {props.movie.title}
-              </Typography>
-            </CardContent>
-          </Card>
+              <CardContent style={{ height: '70px'}}>
+                <Typography variant='subtitle1' style={{ textAlign: 'center' }}>
+                  {props.movie.title}
+                </Typography>
+              </CardContent>
+            </Card>
+          </a>
         </Box>
     )
   }
